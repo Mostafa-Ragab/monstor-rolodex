91fb3d2c26ffe76672e4c7ba344f5252bbd5e56c
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-02 18:51:41 +0200

    making cardlist.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import './App.css';
+import {Cardlist} from './components/card-list/card-list.component';
 class App extends Component {
   constructor() {
     super();
@@ -9,15 +10,19 @@ class App extends Component {
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json)
-    .then(users => this.setState({monsters:users}));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
+
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
+        <Cardlist name='momo'>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name}</h1>
+            ))}
+        </Cardlist>
+
       </div>
     );
   }
--- src/components/cardlist.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const cardlist = props => {
-    return (
-    <div>{props.children}</div>
-    )
-}
-export default cardlist;
