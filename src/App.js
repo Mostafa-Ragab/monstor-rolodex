9f2b8163c490c0f78e2aaa6b31ba04acf6f2d910
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-02 07:32:43 +0200

    making componentdidMount (fetching users)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'sayed',
-          id: 12
-        },
-        {
-          name: 'andrei',
-          id: 13
-        },
-        {
-          name: 'rslan',
-          id: 14
-        }
-      ]
-    
+      monsters: []
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json)
+    .then(users => this.setState({monsters:users}));
   }
   render() {
     return (
