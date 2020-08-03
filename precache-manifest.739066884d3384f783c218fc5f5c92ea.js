7d8d20ddf387345806deb8bcc4ba3d6f9e8521f7
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-08-03 17:26:05 +0200

    Updates

---
+++ precache-manifest.739066884d3384f783c218fc5f5c92ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559837284965c6a71c91c49fbdacadd2",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "80c9f26e83a0577c50fb",
+    "url": "/monstor-rolodex/static/css/main.a9e78fbf.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstor-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstor-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c9f26e83a0577c50fb",
+    "url": "/monstor-rolodex/static/js/main.d1093909.chunk.js"
+  },
+  {
+    "revision": "07e81f7ae69459297abe",
+    "url": "/monstor-rolodex/static/js/runtime-main.582795a9.js"
+  }
+]);
